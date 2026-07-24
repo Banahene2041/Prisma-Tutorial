34bd59ba9b3ba5a5a948efa6dd37eeea16c1eaa7
Gideon <0542342728c@gmail.com>
2026-07-24 18:07:25 +0000

    added the prisma client so it can be used globally across my app and also help with hot reload

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,21 +1,19 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../app/generated/prisma/client";
+import { PrismaClient } from "../../app/generated/prisma/client"; 
+import { PrismaPg } from "@prisma/adapter-pg"; 
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient; 
+}; 
 
-function createPrismaClient() {
-  const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
-  });
-  return new PrismaClient({ adapter });
-}
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL, 
+}); 
 
-export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter, 
+  }); 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
-
-export default prisma;
+export default prisma; 
