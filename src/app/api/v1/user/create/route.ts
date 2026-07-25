2192a966e8acb9b1cadb8a2dcd84659bfc5e993d
Gideon <0542342728c@gmail.com>
2026-07-25 15:28:59 +0000

    learned how to insert many records into database and to prevent dublicate records using the skipDuplicate: true

--- src/app/api/v1/user/create/route.ts
+++ src/app/api/v1/user/create/route.ts
@@ -1,38 +1,64 @@
 import prisma from "@/lib/prisma";
 
-export async function POST(request: Request) {
-  const { name, email } = await request.json();
+// single user with multiple posts and categories
+// export async function POST(request: Request) {
+//   const { name, email } = await request.json();
 
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-      posts: {
-        create: [
-          {
-            title: "Crush Course of Prisma",
-            published: true,
-            content:
-              "This is a crush course of Prisma, you will learn how to use Prisma to create a database and connect to it.",
-            categories: {
-              connectOrCreate: [
-                {
-                  where: { id: 1 },
-                  create: { name: "Prisma" },
-                },
-                {
-                  where: { id: 2 },
-                  create: { name: "Databases" }
-                },
-              ],
-            },
-          },
-        ],
+//   const user = await prisma.user.create({
+//     data: {
+//       name,
+//       email,
+//       posts: {
+//         create: [
+//           {
+//             title: "Crush Course of Prisma",
+//             published: true,
+//             content:
+//               "This is a crush course of Prisma, you will learn how to use Prisma to create a database and connect to it.",
+//             categories: {
+//               connectOrCreate: [
+//                 {
+//                   where: { id: 1 },
+//                   create: { name: "Prisma" },
+//                 },
+//                 {
+//                   where: { id: 2 },
+//                   create: { name: "Databases" }
+//                 },
+//               ],
+//             },
+//           },
+//         ],
+//       },
+//     },
+//   });
+
+//   return new Response(JSON.stringify({ user }), {
+//     status: 200,
+//   });
+// }
+
+// multiple users with multiple posts and categories
+export async function POST() {
+  const users = await prisma.user.createMany({
+    data: [
+      {
+        name: "Enoch Agyemang",
+        email: "enochagyema@gmail.com",
       },
-    },
+      {
+        name: "John Doe",
+        email: "john.doe@example.com",
+      },
+      {
+        name: "Jane Doe",
+        email: "jane.doe@example.com",
+      },
+    ],
+    skipDuplicates: true,
   });
 
-  return new Response(JSON.stringify({ user }), {
+  return new Response(JSON.stringify({ users }), {
     status: 200,
   });
 }
