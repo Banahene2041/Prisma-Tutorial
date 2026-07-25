60b666c2bf6a3d9e66df11631fbcf66552f2972a
Gideon <0542342728c@gmail.com>
2026-07-25 10:14:29 +0000

    learned how to query prisma using aggregation for _sum. which is for adding the total of something

---
+++ prisma/migrations/20260725095912_added_like_count_to_post/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "posts" ADD COLUMN     "likeNum" INTEGER NOT NULL DEFAULT 0;
---
+++ src/app/api/v1/post/aggregation/route.ts
@@ -0,0 +1,13 @@
+import prisma from "@/lib/prisma";
+
+export async function GET() {
+  const aggregations = await prisma.post.aggregate({
+    _sum: {
+      likeNum: true,
+    },
+  });
+
+  return new Response(JSON.stringify(aggregations), {
+    status: 200,
+  });
+}
