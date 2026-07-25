d88eecead07c3d29a21c618f6fc8ea3c0944f565
Gideon <0542342728c@gmail.com>
2026-07-25 10:22:03 +0000

    learned how to use the _min,_max from the aggreation to get the minimum and maximum value of items in a query

--- src/app/api/v1/post/aggregation/route.ts
+++ src/app/api/v1/post/aggregation/route.ts
@@ -2,15 +2,24 @@ import prisma from "@/lib/prisma";
 
 export async function GET() {
   const aggregations = await prisma.post.aggregate({
+    // where:{
+    //     published: true,
+    // },
     _sum: {
       likeNum: true,
     },
-    _avg:{
-        likeNum: true,
+    _avg: {
+      likeNum: true,
     },
-    _count:{
-        id: true,
-    }
+    _count: {
+      id: true,
+    },
+    _max: {
+      likeNum: true,
+    },
+    _min: {
+      likeNum: true,
+    },
   });
 
   return new Response(JSON.stringify(aggregations), {
