f3bf9daa037afcdad633c70949dfda876e2e2df4
Gideon <0542342728c@gmail.com>
2026-07-25 14:38:02 +0000

    learned how to implement cursor based pagination in prisma. cursor based pagination is more scallable than offset pagination becasue as your data grows, wth offset if becomes difficult for your query to go through all the recrod to get you your query as to for cursor based, it uses the records id which are databsed based index and therefore good for querying

--- src/app/api/v1/post/pagination/route.ts
+++ src/app/api/v1/post/pagination/route.ts
@@ -1,14 +1,32 @@
 import prisma from "@/lib/prisma";
 
 // size of page = 10
+// offset based pagination
+// export async function GET(request: Request) {
+//   const { searchParams } = new URL(request.url);
+//   const pgnum = +(searchParams.get("pgnum") ?? 0);
+//   const pgsize = +(searchParams.get("pgsize") ?? 10);
 
+//   const posts = await prisma.post.findMany({
+//     skip: (pgnum - 1) * pgsize,
+//     take: pgsize,
+//   });
+
+//   return new Response(JSON.stringify(posts), {
+//     status: 200,
+//   });
+// }
+
+// cursor based pagination
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const pgnum = +(searchParams.get("pgnum") ?? 0);
+  const cursor = +(searchParams.get("cursor") ?? 0);
   const pgsize = +(searchParams.get("pgsize") ?? 10);
 
   const posts = await prisma.post.findMany({
-    skip: (pgnum - 1) * pgsize,
+    cursor: {
+      id: cursor,
+    },
     take: pgsize,
   });
 
